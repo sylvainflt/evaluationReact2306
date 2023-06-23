fdfeca9a80449c9b3f61785b2fdaa222d6d37e87
sylvainflt <sylvainfoucault1@gmail.com>
2023-06-23 16:12:26 +0200

    nouveau composant Totaux

--- src/component/ListeDepense/index.jsx
+++ src/component/ListeDepense/index.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react"
 import {ListeDepenseContext} from "../../utils/context/ListeDepenseContext"
 import AjoutDepense from "../AjoutDepense"
 import Depense from "../Depense"
+import Totaux from "../Totaux"
 
 function ListeDepense () {
 
-    const [state] = useContext(ListeDepenseContext)
-    const categoryExpenses = Object.keys(state.categoryExpenses)
+    const [state] = useContext(ListeDepenseContext)    
 
     return (
         <div>            
@@ -30,27 +30,10 @@ function ListeDepense () {
                 </div>
                 
             </div>
-            <hr/>
-            <div className="depense">
-                <h2>Totaux : </h2>
-                <span>{state.totalExpenses}€</span>
-                
-                <div>
 
-                    {
-                        
-                        categoryExpenses.map(
-                            (item, key) => 
-                                <div key={key} className="sousTotal">
-                                    <span>{item}</span>
-                                    <span>{state.categoryExpenses[item]}€</span>
-                                </div>
-                        )
-                                                
-                    }
-                    
-                </div>                                                                
-            </div>
+            <hr/>
+
+            <Totaux />
         </div>
     )
 }
---
+++ src/component/Totaux/index.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react"
+import {ListeDepenseContext} from "../../utils/context/ListeDepenseContext"
+
+function Totaux () {
+
+    const [state] = useContext(ListeDepenseContext)
+    const categoryExpenses = Object.keys(state.categoryExpenses)
+
+    return (
+        <div className="depense">
+            <h2>Totaux : </h2>
+            <span>{state.totalExpenses}€</span>
+
+            <div>
+
+                {
+                    
+                    categoryExpenses.map(
+                        (item, key) => 
+                            <div key={key} className="sousTotal">
+                                <span>{item}</span>
+                                <span>{state.categoryExpenses[item]}€</span>
+                            </div>
+                    )
+                                            
+                }
+                
+            </div>                                                                
+        </div>
+    )
+
+}
+
+export default Totaux
